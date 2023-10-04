eb62b5d7c2e0d630fe05e58113a0d76e3839be39
Lukas Holecek <hluk@email.cz>
2023-10-04 08:08:21 +0200

    Update for new requests and urllib3

--- lastfm_wallpaper.py
+++ lastfm_wallpaper.py
@@ -73,7 +73,7 @@ def session():
         connect=3,
         backoff_factor=1,
         status_forcelist=(500, 502, 503, 504),
-        method_whitelist=Retry.DEFAULT_METHOD_WHITELIST.union(("POST",)),
+        allowed_methods=Retry.DEFAULT_ALLOWED_METHODS.union(("POST",)),
     )
     adapter = requests.adapters.HTTPAdapter(max_retries=retry)
     session.mount("http://", adapter)
